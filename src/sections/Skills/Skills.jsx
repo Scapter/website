2973f292de9284f2d49ff6f2f5a89603a95503cd
Scapter <emirnos84@gmail.com>
2026-06-23 00:16:04 +0300

    update-skills

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -14,7 +14,6 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="JavaScript" />
         <SkillList src={checkMarkIcon} skill="HTML" />
         <SkillList src={checkMarkIcon} skill="CSS" />
-        <SkillList src={checkMarkIcon} skill="PHP" />
       </div>
       <hr />
       <div className={styles.skillList}>
